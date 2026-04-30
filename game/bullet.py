9c74190e31d657e21e3783b96612cfa0212d511c
eidam6969 <adamlips26@gmail.com>
2026-04-30 09:05:54 +0200

    oprava lagů

--- game/bullet.py
+++ game/bullet.py
@@ -1,13 +1,17 @@
 import pygame
 from settings import *
-from player import *
+
+bullet_image = None
 
 #načtení objektu kulky hráče, rychlosti objektu a směru
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, player_rect):
         super().__init__()
-        self.image = pygame.image.load("game/img/red_dot.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (14, 14))
+        global bullet_image
+        if bullet_image is None:
+            bullet_image = pygame.image.load("game/img/red_dot.png").convert_alpha()
+            bullet_image = pygame.transform.scale(bullet_image, (14, 14))
+        self.image = bullet_image
         self.rect = self.image.get_rect()
         self.rect.centerx = player_rect.centerx
         self.rect.bottom = player_rect.top - 20
--- game/bullet_invader.py
+++ game/bullet_invader.py
@@ -1,13 +1,17 @@
 import pygame
 from settings import *
-from invader import *
+
+invader_bullet_image = None
 
 #načtení objektu kulky invadera, rychlosti objektu a směru
 class BulletInvader(pygame.sprite.Sprite):
     def __init__(self, invader_rect, speed = 2):
         super().__init__()
-        self.image = pygame.image.load("game/img/green_dot.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (14, 14))
+        global invader_bullet_image
+        if invader_bullet_image is None:
+            invader_bullet_image = pygame.image.load("game/img/green_dot.png").convert_alpha()
+            invader_bullet_image = pygame.transform.scale(invader_bullet_image, (14, 14))
+        self.image = invader_bullet_image
         self.rect = self.image.get_rect()
         self.rect.centerx = invader_rect.centerx
         self.rect.top = invader_rect.bottom
@@ -16,5 +20,5 @@ class BulletInvader(pygame.sprite.Sprite):
 #vymazání objektu po úniku z obrazu
     def update(self):
         self.rect.y += self.speed
-        if self.rect.bottom < 0:
+        if self.rect.top > screen_height:
             self.kill()
